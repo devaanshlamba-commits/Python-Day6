b7664464f26d47832257ce6decd03858e86190d6
devaanshlamba-commits <devaansh.lamba@gmail.com>
2026-06-26 20:54:39 +0530

    first commit

---
+++ Activity2.py
@@ -0,0 +1,6 @@
+string=input("Pls Enter Your Own String: ")
+string1=("")
+for i in string:
+    string1=i+string1
+print("The Original String is: ",string)
+print("The Reversed String is: ",string1)
---
+++ Activity4.py
@@ -0,0 +1,3 @@
+name=("Devaansh")
+for i in name:
+    print(i)
